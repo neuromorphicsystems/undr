7aae85be313e462bc1ac56fbe87c463f03df6aef
Alexandre Marcireau <alexandre.marcireau@gmail.com>
2021-06-16 14:50:26 +0200

    Add a basic Python package project structure

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# undr
+<p align="center">
+    <img src="undr.png" width="256">
+</p>
+
+# Unified Neuromorphic Datasets Repository
+
+Get your data down UNDR.
+
+## Python package
+
+```sh
+pip3 install undr
+```
+
+## Dataset format specification
+
+## Dataset mirrors
+
+
+## Publish
+
+1. Bump the version number in *setup.py*.
+
+2. Install twine
+```
+pip3 install twine
+```
+
+3. Upload the source code to PyPI:
+```
+python3 setup.py sdist
+python3 -m twine upload dist/*
+```
---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open('README.md') as file:
+    long_description = file.read()
+
+setuptools.setup(
+    name='undr',
+    version='0.0.1',
+    url='https://github.com/neuromorphicsystems/undr',
+    author='Alexandre Marcireau',
+    author_email='alexandre.marcireau@gmail.com',
+    description='Download and process Neuromorphic datasets',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=[
+        'lzip>=1.0',
+        'numpy>=1.20',
+        'toml>=0.10',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+    ],
+    package_dir={'': 'undr'},
+    packages=setuptools.find_packages(where='src'))
