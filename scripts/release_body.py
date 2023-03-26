0ec180560e3f5e7d1d1e2e3d39a579765061dc2b
Alexandre Marcireau <alexandre.marcireau@gmail.com>
2023-03-26 19:09:35 +1100

    [release] Escape line breaks

--- scripts/release_body.py
+++ scripts/release_body.py
@@ -4,7 +4,7 @@ import version
 
 sys.stdout.write(
     "".join(
-        f"{line}\n"
+        f"{line}%0A"
         for line in (
             f"- app v{version.version_to_string(version.app_version)} (see Assets below)",
             f"- python v{version.version_to_string(version.python_version)} (https://pypi.org/project/undr/)",
