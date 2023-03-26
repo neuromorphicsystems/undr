82766f63f651ab5b7944f320a4b262255cb3fece
Alexandre Marcireau <alexandre.marcireau@gmail.com>
2023-03-26 19:55:49 +1100

    [release] Try with json

--- scripts/release_body.py
+++ scripts/release_body.py
@@ -1,14 +1,17 @@
+import json
 import sys
 
 import version
 
 sys.stdout.write(
-    "".join(
-        f"{line}%0A"
-        for line in (
-            f"- app v{version.version_to_string(version.app_version)} (see Assets below)",
-            f"- python v{version.version_to_string(version.python_version)} (https://pypi.org/project/undr/)",
-            f"- rust v{version.version_to_string(version.rust_version)} (https://crates.io/crates/undr/)",
+    json.dumps(
+        "".join(
+            f"{line}\n"
+            for line in (
+                f"- app v{version.version_to_string(version.app_version)} (see Assets below)",
+                f"- python v{version.version_to_string(version.python_version)} (https://pypi.org/project/undr/)",
+                f"- rust v{version.version_to_string(version.rust_version)} (https://crates.io/crates/undr/)",
+            )
         )
     )
 )
