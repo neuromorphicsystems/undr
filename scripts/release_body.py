1b09ef438f9c7523476d0a29cb7070e5d6146b1b
Alexandre Marcireau <alexandre.marcireau@gmail.com>
2023-03-26 20:48:01 +1100

    Use recommendation from https://github.com/orgs/community/discussions/26288

--- scripts/release_body.py
+++ scripts/release_body.py
@@ -4,14 +4,12 @@ import sys
 import version
 
 sys.stdout.write(
-    json.dumps(
-        "".join(
-            f"{line}\n"
-            for line in (
-                f"- app v{version.version_to_string(version.app_version)} (see Assets below)",
-                f"- python v{version.version_to_string(version.python_version)} (https://pypi.org/project/undr/)",
-                f"- rust v{version.version_to_string(version.rust_version)} (https://crates.io/crates/undr/)",
-            )
+    "".join(
+        f"{line}\n"
+        for line in (
+            f"- app v{version.version_to_string(version.app_version)} (see Assets below)",
+            f"- python v{version.version_to_string(version.python_version)} (https://pypi.org/project/undr/)",
+            f"- rust v{version.version_to_string(version.rust_version)} (https://crates.io/crates/undr/)",
         )
     )
 )
