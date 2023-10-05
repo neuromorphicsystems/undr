2758e625b321e6e0b2b0acac6a99375a1dfca1c8
Alexandre Marcireau <alexandre.marcireau@gmail.com>
2023-10-05 13:55:20 +1100

    [release] Bump versions

--- scripts/release_body.py
+++ scripts/release_body.py
@@ -6,9 +6,9 @@ sys.stdout.write(
     "".join(
         f"{line}\n"
         for line in (
-            f"- app v{version.version_to_string(version.app_version)} (see Assets below)",
-            f"- python v{version.version_to_string(version.python_version)} (https://pypi.org/project/undr/)",
-            f"- rust v{version.version_to_string(version.rust_version)} (https://crates.io/crates/undr/)",
+            f"- app {version.version_to_string(version.app_version)} (see Assets below)",
+            f"- python {version.version_to_string(version.python_version)} (https://pypi.org/project/undr/)",
+            f"- rust {version.version_to_string(version.rust_version)} (https://crates.io/crates/undr/)",
         )
     )
 )
